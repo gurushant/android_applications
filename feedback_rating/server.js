fbd1d3313e2ef12dec9fa530b493d4221eaabc25
Cloud User <ec2-user@ip-172-31-39-211.us-west-2.compute.internal>
2016-11-16 06:49:29 -0500

    added new rest service to post feedback

--- feedback_rating/server.js
+++ feedback_rating/server.js
@@ -34,23 +34,45 @@ app.listen(PORT_NO,function(){
 
 
 
-/*
-*Following api accepts the order id,fetches order details from database and responds back to the server.
-*/
+ /**
+ *This function is for
+ * 1.To fetch order details by calling method from common utils.
+ * 2.If order does not exist then respond with appropriate message.  
+ */
 app.get('/orders/getOrderDetail',function(req,res)
 {
 	var orderId=req.query.order_id;
 	logger.info('Getting order details of '+orderId);
+	res.header("Access-Control-Allow-Origin", "*");
 	commonUtils.getOrderDetail(orderId,function(rows){
+		var response=null;
 		if(rows.length==1){
-			var response=rows[0];
+			response=rows[0];
+			console.log('response >>>>>>>>>>>>'+response.toString());
 			response=JSON.stringify(response);
-			logger.debug('order details of '+orderId+' is '+response);
-			res.send(response);	
+			response=JSON.parse(response);
+			response.status='SUCCESS';
+			response.message='Order detail fetched successfully';
+//			response=JSON.stringify(response);
+			logger.info('order details of '+orderId+' is '+response);
 		}
+		else
+		{	//If order does not exist in database
+			logger.debug('Order does not exist =>'+orderId);
+			response="{ \"status\":\"ERROR\", \"message\":\"Order not found\"}";	
+		}
+		res.json(response);
 	});	
 });
 
+/**
+ *This function is for
+ * 1.If feedback is already received then respond with message appropriate message.
+ * 2.If feedback does not exist then call method of common utils to save the feedback and rating payload.
+ * 3.Calculate overall rating of recipe and add json element into the payload.
+ * 4.Calculate overall rating of order including overall recipe rating and insert json element for order's overall rating into the payload.
+ * 5.Error handling if any exception occurs.
+ */
 
 app.post('/review/post_feedback',function(req,res)
 {	
@@ -67,12 +89,15 @@ app.post('/review/post_feedback',function(req,res)
 	var receipeLength=recipeArray.length;
 	logger.info('Recipe array length is '+receipeLength);
 	var ratingSum=0;
+	//Sum of all recipies in order.
 	for(var k=0;k <receipeLength;k++)
 	{
 		ratingSum+=parseFloat(JSON.stringify(recipeArray[k]).split(':')[1].replace('}',''));
 	}
 	logger.info('Rating sum is '+ratingSum);
+	//Calculating overall rating of recipes in order
 	var overallRecipeRating=ratingSum/receipeLength;
+	//Calculating overall rating of an order.
 	var overAllRating=(overallRecipeRating+parseFloat(economy)+parseFloat(ambience)+parseFloat(qos))/4;
 	logger.info('Final recipe rating is '+overallRecipeRating);
 	logger.info('Final rating of order id '+overAllRating);
@@ -80,26 +105,55 @@ app.post('/review/post_feedback',function(req,res)
 	req.body.overall_order_rating=overAllRating;
 	var feedbackPayload=JSON.stringify(req.body);
 	logger.info('Feedback payload is '+feedbackPayload);
-
+	//Checking whether order has already recived feedback and rating .
 	logger.info('Checking whether is already received.');
 	commonUtils.checkFeedbackExist(orderId,restaurantId,function(err,response){
-		console.log('>>>>>>>>>>>>>>>>>>>>>'+response);
+		logger.info('Checking whether feedback is already received for this order '+orderId+'=>'+response);
+		//If order does not have feedback and rating recived.This is the first time.
 		if(response==false)
 		{
+			logger.info('Updating email notifications table');
+			commonUtils.updateEmailNotifications(orderId,restaurantId,function(err,response)
+			{
+				if(err==null)
+				{
+					logger.log('Email notification table is updated');
+					logger.log('Updating orders table');
+					commonUtils.updateOrders(feedbackPayload,orderId,restaurantId,function(err,response)
+					{
+						if(err!=null)
+						{
+							logger.error('Error occured while updating orders table =>'+err);
+							 res.json("{ \"status\": \"error\", \"message\": \"Error occured while updating feedback and rating.\" }");
+						}
+						else
+						{
+							logger.info('Order updated successfully');
+			         			 res.json("{ \"status\": \"success\", \"message\": \"Feedback updated successfully.\" }");
+						}
+					});
+				}
+				else
+				{
+					logger.error('Error occrued while updating email notification table =>'+err);	
+					res.json("{ \"status\": \"error\", \"message\": \"Error occured while updating feedback and rating.\" }");
+				}
+
+			});	
+		}
+		else
+		{
+			        res.json("{ \"status\": \"success\", \"message\": \"Feedback already exist for this order\" }");
+	
 		}
 	
 	});	
-	 commonUtils.postFeedbackAndRating(feedbackPayload,orderId,restaurantId,function(err,response){
-			if(err!=null)
-			{
-				logger.error('Error occured while inserting feedback and rating.Error is =>'+err);
-				res.json("{ \"status\": \"failed\", \"message\": \"Oops,Error occured while processing feedback.Try again later.\" }");
-			}
-			else
-			{
-				 res.json("{ \"status\": \"success\", \"message\": \"!!Thanks,Successfully posted feedback and rating of the order\" }");
 
-			}
-        });
+});
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
--- feedback_rating/utils/commonUtils/commonMethods.js
+++ feedback_rating/utils/commonUtils/commonMethods.js
@@ -31,32 +31,41 @@ exports.getOrderDetail=function(orderId,callback)
         });
 }
 
-exports.postFeedbackAndRating=function(feedbackPayload,orderId,restaurantId,callback)
-{
-	 
-	var query="select *from email_notification  where id='"+orderId+"' and  restaruent_id='"+restaurantId+"'";
-        dbCon.query(query ,function(err,rows)
-	{
-	      if(rows.length >0 )
-		{
-		callback(err,'ALREADY_FEEDBACK_EXIST');
-		return;
-		}
-	});	
 
-	var query="insert into email_notification(payload,order_id,restaruent_id,email_sent_time,is_feedback_rating_received) values('"+feedbackPayload+"','"+orderId+"','"+restaurantId+"',now(),true)";
-	logger.info('posting feedback and rating '+query);
-	 dbCon.query(query ,function(err,response)
-        {
-                callback(err,response);
-        });
-}
+
+exports.updateEmailNotifications=function(orderId,restaurantId,callback)
+	{
+		var emailUpdateQuery=" update email_notification set is_feedback_rating_received=true,feedback_received_time=now() where order_id="+orderId+" and restaruent_id="+restaurantId;
+		logger.info('Email notification update query is =>'+emailUpdateQuery);
+		 dbCon.query(emailUpdateQuery ,function(err,response)
+	         {
+         	       callback(err,response);
+       		 });
+
+	}
+
+exports.updateOrders=function(feedbackPayload,orderId,restaurantId,callback)
+	{	
+	        var orderUpdateQuery=" update orders set rating_feedback_data='"+feedbackPayload+"' where id="+orderId+" and restaruent_id="+restaurantId;
+		logger.info('Order update query is=>'+orderUpdateQuery);
+		 dbCon.query(orderUpdateQuery ,function(err,response)
+                 {
+                       callback(err,response);
+                 });
+	}
 
 exports.checkFeedbackExist=function(orderId,restaurantId,callback)
 	{
-	 var query="select *from email_notification  where order_id='"+orderId+"' and  restaruent_id='"+restaurantId+"'";
+	 var query="select *from email_notification  where order_id='"+orderId+"' and  restaruent_id='"+restaurantId+"' and is_feedback_rating_received=true";
+	logger.info('email notification check query is=>'+query);
 	dbCon.query(query ,function(err,rows)
         {
+		if(err!=null)
+		{
+			logger.error('Error occured while checking order status=>'+err);
+			callback(err,false);
+			return;
+		}
               if(rows.length >0 )
                 {
       	          callback(err,true);
