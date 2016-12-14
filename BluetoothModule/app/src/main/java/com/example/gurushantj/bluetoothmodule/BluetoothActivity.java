fc417b4086c87129d8155f00927c27014b656421
gurushant.jidagi <gurushant.jidagi@afourtech.com>
2016-12-14 14:05:01 +0530

    bluetooth android application

--- BluetoothModule/app/src/main/java/com/example/gurushantj/bluetoothmodule/BluetoothActivity.java
+++ BluetoothModule/app/src/main/java/com/example/gurushantj/bluetoothmodule/BluetoothActivity.java
@@ -1,28 +1,20 @@
 package com.example.gurushantj.bluetoothmodule;
 
-import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.os.Environment;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Set;
@@ -52,73 +44,85 @@ public class BluetoothActivity extends AppCompatActivity  {
            Toast.makeText(getApplicationContext(),"Error occured while creating a file",Toast.LENGTH_LONG ).show();
        }
 
-        bluetoothLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                validateBlutoothLogin();
-            }
-        });
-    }
-
-    private void validateBlutoothLogin()
-    {
+        //Init bluetooth device
         Set<BluetoothDevice>pairedDevices=null;
         bluetoothAdapter= BluetoothAdapter.getDefaultAdapter();
         if(bluetoothAdapter==null)
         {
             Toast.makeText(getApplicationContext(),"Device does not support for bluetooth",Toast.LENGTH_LONG).show();
         }
-        else
-        {
-            if(!bluetoothAdapter.isEnabled())
-            {
-                Toast.makeText(getApplicationContext(),"Please enable your bluetoth",Toast.LENGTH_LONG).show();
-            }
-            else
-            {
-                pairedDevices=bluetoothAdapter.getBondedDevices();
-                Toast.makeText(getApplicationContext(),"Paired devices "+pairedDevices.size(),Toast.LENGTH_LONG).show();
-                for(BluetoothDevice blueDevice:pairedDevices)
-                {
-                    if(blueDevice.getName().equals("HC-05")) {
+        else {
+            if (!bluetoothAdapter.isEnabled()) {
+                Toast.makeText(getApplicationContext(), "Please enable your bluetoth", Toast.LENGTH_LONG).show();
+            } else {
+                pairedDevices = bluetoothAdapter.getBondedDevices();
+                Toast.makeText(getApplicationContext(), "Paired devices " + pairedDevices.size(), Toast.LENGTH_LONG).show();
+                for (BluetoothDevice blueDevice : pairedDevices) {
+                    if (blueDevice.getName().equals("HC-05")) {
                         bluetoothDevice = blueDevice;
-                        Toast.makeText(getApplicationContext(),"Found targeted bluettoth device=>"+bluetoothDevice.getName(),Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "Found targeted bluetooth device=>" + bluetoothDevice.getName(), Toast.LENGTH_LONG).show();
                         break;
                     }
 
                 }
-                if(bluetoothDevice!=null)
-                {
+                if (bluetoothDevice != null) {
                     try {
                         blueSocket = bluetoothDevice.createRfcommSocketToServiceRecord(applicationUID);
                         bluetoothAdapter.cancelDiscovery();
                         blueSocket.connect();
                         outputStream = blueSocket.getOutputStream();
                         inputStream = blueSocket.getInputStream();
-                    }
-                    catch (Exception ex)
-                    {
+                    } catch (Exception ex) {
                         ex.printStackTrace();
                     }
 
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            writeMessage("test");
-                        }
-                    }).start();
 
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            readMessage();
-                        }
-                    }).start();
                 }
 
             }
         }
+        //end of bluetooth device
+        bluetoothLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendBluetoothMessage();
+            }
+        });
+
+        //Read message from bluetooth
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                readMessage();
+            }
+        }).start();
+
     }
+
+    private void showToast(final byte bytes[])
+    {
+        final String message=new String(bytes);
+
+            this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(BluetoothActivity.this,message,Toast.LENGTH_LONG).show();
+             }
+        });
+    }
+    private void sendBluetoothMessage()
+    {
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    writeMessage("test");
+                }
+            }).start();
+
+        }
+
 
     public void readMessage()
     {
@@ -129,10 +133,19 @@ public class BluetoothActivity extends AppCompatActivity  {
 
         while (true) {
             try {
+             //   Toast.makeText(getApplicationContext(),"Listening to the port for data read",Toast.LENGTH_LONG ).show();
                 bytes = inputStream.read(buffer, bytes, BUFFER_SIZE - bytes);
-                Toast.makeText(getApplicationContext(),buffer.length,Toast.LENGTH_LONG).show();
-            } catch (IOException e) {
+                showToast(buffer);
+               // Toast.makeText(getApplicationContext(),bytes,Toast.LENGTH_LONG).show();
+            } catch (Throwable e) {
                 e.printStackTrace();
+                Toast.makeText(getApplicationContext(),"Error occured while sending data through bluetooth",Toast.LENGTH_LONG ).show();
+                StringWriter swr=new StringWriter();
+                PrintWriter print=new PrintWriter(swr);
+                e.printStackTrace(print);
+                writeToFile(swr.toString());
+                closeFile();
+
             }
         }
 
